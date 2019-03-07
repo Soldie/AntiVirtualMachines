57960095963ae2887b02cfb78fdc9ad61511ab30
Antlion <40786755+r3xq1@users.noreply.github.com>
2019-03-08 00:34:51 +0300

    Add files via upload

---
+++ AntiVM/NativeMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace AntiVM
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    internal static class NativeMethods
+    {
+        [DllImport("kernel32.dll", BestFitMapping = false, CharSet = CharSet.Unicode)]
+        internal static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
---
+++ AntiVM/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AntiVM
+{
+    using System;
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.Title = "Developer by Antlion";
+            if (!AntiVM.GetCheckVMBot())
+            {        
+                Console.WriteLine("Программа запущена вне виртуальных машин!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Внимание - Обнаружена виртуальная машина.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
