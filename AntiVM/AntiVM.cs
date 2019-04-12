672dc573ae59bc6e1ef2715d4521e81b054fe7cb
Antlion <40786755+r3xq1@users.noreply.github.com>
2019-04-13 01:44:31 +0300

    Update AntiVM.cs

--- AntiVM/AntiVM.cs
+++ AntiVM/AntiVM.cs
@@ -1,4 +1,4 @@
-﻿namespace AntiVM
+namespace AntiVM
 {
     using System;
     using System.Diagnostics;
@@ -9,26 +9,41 @@
     {
         private static bool GetDetectVirtualMachine()
         {
-            using (ManagementObjectCollection mob = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem").Get())
+            using (ManagementObjectCollection managementObject = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem").Get())
             {
-                foreach (ManagementBaseObject xa in mob)
+                foreach (ManagementBaseObject managementBaseObject in managementObject)
                 {
                     try
                     {
-                        string str = xa["Manufacturer"].ToString().ToLower();
-                        bool strTo = xa["Model"].ToString().ToUpperInvariant().Contains("VIRTUAL");
-                        if ((str.Equals("microsoft corporation") && strTo) || str.Contains("vmware") || xa["Model"].ToString().Equals("VirtualBox"))
+                        string str = managementBaseObject["Manufacturer"].ToString().ToLower();
+                        bool strTo = managementBaseObject["Model"].ToString().ToLower().Contains("virtual");
+                        if ((str.Equals("microsoft corporation") && strTo) || str.Contains("vmware") || managementBaseObject["Model"].ToString().Equals("VirtualBox"))
                         {
                             return true;
                         }
                     }
-                    catch (ArgumentNullException) { return false; }
+                    catch (Exception) { return false; }
                 }
             }
             return false;
         }
+
+        private static bool IsDebuggerAttached(Process process)
+        {
+            try
+            {
+                bool isDebuggerAttached = false;
+                NativeMethods.CheckRemoteDebuggerPresent(process.Handle, ref isDebuggerAttached);
+                return isDebuggerAttached;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
         private static bool IsRdpAvailable => SystemInformation.TerminalServerSession == true ? true : false;
         private static bool SBieDLL() => Process.GetProcessesByName("wsnm").Length <= 0 && NativeMethods.GetModuleHandle("SbieDll.dll").ToInt32() == 0 ? false : true;
-        public static bool GetCheckVMBot() => SBieDLL() || IsRdpAvailable || GetDetectVirtualMachine() ? true : false;
+        public static bool GetCheckVMBot() => IsDebuggerAttached(Process.GetCurrentProcess()) || SBieDLL() || IsRdpAvailable || GetDetectVirtualMachine() ? true : false;
     }
 }
