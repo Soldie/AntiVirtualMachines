a34942dd4c6a32323752251e63e49fa1800a0e93
Antlion <40786755+r3xq1@users.noreply.github.com>
2019-04-13 01:45:35 +0300

    Update NativeMethods.cs

--- AntiVM/NativeMethods.cs
+++ AntiVM/NativeMethods.cs
@@ -1,4 +1,4 @@
-﻿namespace AntiVM
+namespace AntiVM
 {
     using System;
     using System.Runtime.InteropServices;
@@ -7,5 +7,9 @@
     {
         [DllImport("kernel32.dll", BestFitMapping = false, CharSet = CharSet.Unicode)]
         internal static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("Kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool CheckRemoteDebuggerPresent(IntPtr hProcess, [MarshalAs(UnmanagedType.Bool)]ref bool isDebuggerPresent);
     }
 }
